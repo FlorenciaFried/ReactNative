0211b5b7c8b333f8523a1b56085935dcb1cfec26
FlorenciaFried <florencia.d.fried@gmail.com>
2020-10-12 16:31:28 -0300

    Proyecto de Criptomonedas finalizado

--- criptomonedas/App.js
+++ criptomonedas/App.js
@@ -1,17 +1,84 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
+  Image,
   ScrollView,
+  ActivityIndicator,
   View,
   Text,
   StatusBar,
 } from 'react-native';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Cotizacion from './components/Cotizacion';
+import axios from 'axios';
 
 const App = () => {
-  return <></>;
+  const [moneda, setMoneda] = useState('');
+  const [criptomoneda, setCriptomoneda] = useState('');
+  const [consultarAPI, setConsultarAPI] = useState(false);
+  const [resultado, setResultado] = useState({});
+  const [cargando, setCargando] = useState(false);
+
+  useEffect(() => {
+    const cotizarCriptomoneda = async () => {
+      if (consultarAPI) {
+        // consultar la api para obtener la cotización
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+        const resultado = await axios.get(url);
+
+        // console.log(resultado.data.DISPLAY[criptomoneda][moneda] );
+        setCargando(true);
+
+        // Ocultar el spinner y mostrar el resultado
+        setTimeout(() => {
+          setResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+          setConsultarAPI(false);
+          setCargando(false);
+        }, 3000);
+      }
+    };
+    cotizarCriptomoneda();
+  }, [consultarAPI]);
+
+  const componente = cargando ? (
+    <ActivityIndicator size="large" color="#5E49E2" />
+  ) : (
+    <Cotizacion resultado={resultado} />
+  );
+  return (
+    <ScrollView>
+      <Header />
+
+      <Image
+        style={styles.imagen}
+        source={require('./assets/img/cryptomonedas.png')}
+      />
+      <View style={styles.contenido}>
+        <Formulario
+          moneda={moneda}
+          criptomoneda={criptomoneda}
+          setMoneda={setMoneda}
+          setCriptomoneda={setCriptomoneda}
+          setConsultarAPI={setConsultarAPI}
+        />
+      </View>
+
+      <View style={{marginTop: 40}}>{componente}</View>
+    </ScrollView>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagen: {
+    width: '100%',
+    height: 150,
+    marginHorizontal: '2.5%',
+  },
+  contenido: {
+    marginHorizontal: '2.5%',
+  },
+});
 
 export default App;
